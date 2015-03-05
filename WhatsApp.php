0af2e6966095bd8f2aab8e0823bb9834d4cdf2ad
andy <andy@andyf>
2015-03-06 00:05:41 +0700

    Core Engine

--- WhatsApp.php
+++ WhatsApp.php
@@ -2,8 +2,31 @@
 
 namespace sintret\whatsapp;
 
-require 'src/whatsprot.class.php';
+include 'src/whatsprot.class.php';
 
-class WhatsApp extends WhatsProt {
-    
+class WhatsApp {
+
+    public $number = 6281311160876;
+    public $app = 'Alfarecords';
+    public $password = 'c7v3YhuBR1QfZhe/5NDRn4YZxnk=';
+    public $connect;
+
+    public function __construct() {
+        $this->getConnection();
+    }
+
+    public function getConnection() {
+        $w = new \WhatsProt($this->number, 0, $this->app, true);
+        $w->connect();
+        $w->loginWithPassword($password);
+        $this->connect = $w;
+    }
+
+    public function send($target, $message) {
+        $target = '6281575068530'; //Target Phone,reciever phone 
+        $message = 'Your message comes here';
+        $this->connect->SendPresenceSubscription($target); //Let us first send presence to user 
+        $this->connect->sendMessage($target, $message); // Send Message 
+    }
+
 }
